fbaf782ad7814c5be95ff8e027804f0a5c1e9929
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-01-23 15:26:03 -0600

    udemy -> arreglo bidimensional completado

--- udemy/arraysBidimensionales.c
+++ udemy/arraysBidimensionales.c
@@ -12,16 +12,19 @@
 #include <stdio.h>
 
 int main(){
-    const int filas = 2, columnas = 3;
-    int tabla[filas][columnas] = {1,2,3,4,5,6};
+    const int filas = 7, columnas = 8;
+    int tabla[filas][columnas] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56};
     //int tabla[filas][columnas] = {{1,2,3},{4,5,6}};
 
-    for (int i = 0; i < 2; i++)
+    printf("%i X %i = %i:\n\n", filas, columnas, (filas * columnas));
+
+    for (int i = 0; i < filas; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < columnas; j++)
         {
-            printf("%i ", tabla[i][j]);
+            printf("%i\t", tabla[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
 }
--- udemy/proofs.c
+++ udemy/proofs.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
+void punterosYCadenas();
+void arreglosDeCaracteres();
+
+int main(){
+    punterosYCadenas();
+    arreglosDeCaracteres();
+
+    return 0;
+}
+
 // Para ver si devuelve el tamaño completo del arreglo a lo que
 // apunta el puntero
-int main (){
+void punterosYCadenas(){
     char texto[] = "Esto es una pinche mamada bien chueca";
     char *puntero = (char *)texto;
     printf("tamaño desde el puntero: %lu\n", strlen(puntero));
     printf("tamaño desde el arreglo: %lu\n", strlen(texto));
-
-    return 0;
 }
 // CONCLUSIÓN: Sí lo devuelve bien
 
-// 
+// Para saber cómo se imprime un arreglo de caracteres
+void arreglosDeCaracteres(){
+    char arr[3] = {'0', 64, '@'};
+
+    printf("Imprimo el arreglo de Caracteres:\n");
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%c ", arr[i]);
+    }
+    printf("\n");
+}
+// CONCLUSIÓN: Se imrpime la arroba dos veces por su código ASCII
