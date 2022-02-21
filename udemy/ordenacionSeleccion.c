89654f8f81ff3ad48a7a9608213ed0b637e16a96
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-02-21 16:48:58 -0600

    udemy -> empezando metodo ordenacion por insercion

--- udemy/ordenacionSeleccion.c
+++ udemy/ordenacionSeleccion.c
@@ -1,57 +1,56 @@
 /**
- * @file ordenacionIntercambio.c
+ * @file ordenacionSeleccion.c
  * @author Omar I. Martínez Gómez (omar_238@hotmail.com)
- * @brief Algoritmo de Ordenación por Intercambio
+ * @brief Algoritmo de Ordenación por Selección
  * @version 0.1
  * @date 2022-02-20
  * Ejercicio del libro "Programación en Java 2, Algoritmos, Estructura
- * de datos y Programación orientada a objetos"; Joyantes Aguilar, Luis;
+ * de datos y Programación orientada a objetos"; Joyantes Aguilar, Luis; 
  * Zahonero Martínez, Ignacio; Editorial McGraw-Hill
  * @copyright Copyright (c) 2022
- *
+ * 
  */
 #include <stdio.h>
 #define TAMANO 10
 
-void imprimirArreglo(int arreglo[], int size);
-void ordenarXIntercambio(int arreglo[], int size);
+void imprimirArreglo(int arreglo[],int size);
+void ordenarXSeleccion(int arreglo[],int size);
 
-int main()
-{
-    int a[TAMANO] = {9, 7, 5, 3, 1, 0, 8, 6, 4, 2};
+int main(){
+    int a[TAMANO] = {9,7,5,3,1,0,8,6,4,2};
 
     printf("Impresión del arreglo en orden original:\n");
     imprimirArreglo(a, TAMANO);
 
-    ordenarXIntercambio(a, TAMANO);
+    ordenarXSeleccion(a, TAMANO);
 
     printf("Impresión del arreglo ordenado:\n");
     imprimirArreglo(a, TAMANO);
-
+    
     return 0;
 }
 
-void imprimirArreglo(int arreglo[], int size)
-{
+void imprimirArreglo(int arreglo[], int size){
     for (int i = 0; i < size; i++)
         printf("%i ", arreglo[i]);
     printf("\n");
 }
 
-void ordenarXIntercambio(int arreglo[], int size)
-{
-    int swp;
+void ordenarXSeleccion(int arreglo[], int size){
+    int swp, menor;
 
     for (int i = 0; i <= size - 2; i++)
     {
+        menor = i;
         for (int j = i + 1; j <= size - 1; j++)
+            if (arreglo[menor] > arreglo[j])
+                menor = j;
+
+        if (i != menor)
         {
-            if (arreglo[i] > arreglo[j])
-            {
-                swp = arreglo[i];
-                arreglo[i] = arreglo[j];
-                arreglo[j] = swp;
-            }
+            swp = arreglo[menor];
+            arreglo[menor] = arreglo[i];
+            arreglo[i] = swp;
         }
     }
 }
