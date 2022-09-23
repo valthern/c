0e99a0ad3feccd5613b082e7812eebc5ac38e1dd
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-09-23 14:05:45 -0500

    libro -> adicion de programa de char comparado con int

---
+++ libro/cap04/program.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    // asigno a c el valor 65 ('A' es sinonimo de 65)
+    char c= 'A';
+    printf("Como caracter: %c \n",c);
+    printf("Valor numerico ASCII: %d\n",c);
+    // asigno a i el valor 65
+    int i='A';
+    printf("Como caracter: %c \n",i);
+    printf("Valor numerico ASCII: %d\n",i);
+
+    return 0;
+}
