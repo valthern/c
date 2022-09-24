f46677ec41bfae8033098991ca73cc4ac9a5db72
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-09-24 15:59:37 -0500

    libro -> archivo de funciones

---
+++ libro/cap04/funciones.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int esDigito(char c)
+{
+    return c >= '0' && c <= '9';
+}
+
+int esLetra(char c)
+{
+    return c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z';
+}
+
+int esMayuscula(char c){
+    return c>='A'&&c<='Z';
+}
+
+int esMinuscula(char c){
+    return c>='a'&&c<='z';
+}
+
+char aMinuscula(char c){
+    return esMayuscula(c)?c-'A'+'a':c;
+}
+
